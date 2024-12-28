5404af823459e06f5a5b1c9d587d7d8e42abf632
Hung <100249698+huhgque@users.noreply.github.com>
2024-12-28 15:30:37 +0700

    Add files via upload

--- keybind.py
+++ keybind.py
@@ -3,36 +3,41 @@
 import os
 
 def main():
-    ini_file_check = ["merged.ini","Firefly.ini"]
+    exclude_file = ["RabbitFX.ini"]
     # traverse root directory, and list directories as dirs and files as files
     for root, dirs, files in os.walk("."):
         path = root.split(os.sep)
         # print((len(path) - 1) * '---', os.path.basename(root))
         for file in files:
-            for ini_name in ini_file_check:
-                if file.__contains__(ini_name) :
-                    result = ini_write(root+"\\"+file)
-                    if result == 1 :
-                        print("Done "+root)
-                    else :
-                        print("Fail "+root)
+            if exclude_file.__contains__(file):
+                continue
+            if file.__contains__(".ini") and not file.__contains__("DISABLED"):
+                print(file)
+                result = ini_write(root+"\\"+file)
+                if result == 1 :
+                    print("Done "+root)
+                else :
+                    print("Fail "+root)
+    print("Press any to quit")
+    wait = input()
 
 
 def ini_write(path):
     f = open(path,"r")
     controll_key = ["alt","ctrl"]
-    controll_key_index = 0
-    current_controll_key_use = controll_key[controll_key_index]
     counter = 1
+    cycle = 0
     lines = []
+    control_1 = controll_key[0]
+    control_2 = "no_" + controll_key[1]
     for line in f:
         if line.__contains__("key = "):
-            lines.append( "key = "+current_controll_key_use+" "+counter.__str__()+"\n")
+            key_command = "{} {} {}\n"
+            lines.append( "key = "+key_command.format(control_1,control_2,counter))
             counter+=1
             if counter > 9:
                 counter = 1
-                controll_key_index += 1
-                current_controll_key_use += " "+controll_key[controll_key_index]
+                control_2 = controll_key[1]
         else :
             lines.append(line)
     f.close()
